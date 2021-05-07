5ff74f8f6b69d1ad97ab8b4ab460b8259fa22092
ayfokin <ayfokin@mail.ru>
2021-05-07 19:51:33 +0300

    Added css

--- MusicalEarTrainer/views.py
+++ MusicalEarTrainer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from catalog.models import Exercise
 
+
 def home(request):
     exercises = Exercise.objects
     return render(request, 'home.html', {'exercises': exercises})
