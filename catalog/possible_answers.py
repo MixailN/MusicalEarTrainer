9d50a94da56152fcfc7547e9e4ae20d341ad8d1f
Michael <mixail_nabiullin@mail.ru>
2021-05-13 09:27:47 +0300

    Fixed bug

--- catalog/possible_answers.py
+++ catalog/possible_answers.py
@@ -11,12 +11,19 @@ ANSWERS_QUANTITY = 4
 def create_form(task_type, correct_answer):
     possible_answers = list(POSSIBLE_ANSWERS[task_type])
     result_answers = []
-    possible_answers.remove(correct_answer)
-    for i in range(ANSWERS_QUANTITY - 1):
-        index = random.randint(0, len(possible_answers) - 1)
-        result_answers.append([possible_answers[index], possible_answers[index]])
-        possible_answers.pop(index)
-    index = random.randint(0, len(result_answers) - 1)
-    result_answers.insert(index, [correct_answer, correct_answer])
-    form = AnswerForm(result_answers)
+    if task_type == 1:
+        result_answers.append([correct_answer, correct_answer])
+        index = random.randint(0, len(possible_answers))
+        possible_answers.remove(correct_answer)
+        result_answers.insert(index, [possible_answers[0], possible_answers[0]])
+        form = AnswerForm(result_answers)
+    else:
+        possible_answers.remove(correct_answer)
+        for i in range(ANSWERS_QUANTITY - 1):
+            index = random.randint(0, len(possible_answers) - 1)
+            result_answers.append([possible_answers[index], possible_answers[index]])
+            possible_answers.pop(index)
+        index = random.randint(0, len(result_answers) - 1)
+        result_answers.insert(index, [correct_answer, correct_answer])
+        form = AnswerForm(result_answers)
     return form
